5c7fcd20620c3802420b124086394c13321a46e0
Matt Hand <matt.kaze3@gmail.com>
2018-10-25 15:53:56 +0100

    Enable more ivy commands

--- init.el
+++ init.el
@@ -15,7 +15,12 @@
 
   (require 'ivy)
   (ivy-mode 1)
-  (global-set-key (kbd "C-s") 'swiper))
+  (setq ivy-use-virtual-buffers t)
+  (setq enable-recursive-minibuffers t)
+  (global-set-key (kbd "\C-s") 'swiper)
+  (global-set-key (kbd "\C-x \C-f") 'counsel-find-file)
+  (global-set-key (kbd "\M-x") 'counsel-M-x)
+)
 
 (setq custom-file "~/.emacs.d/custom.el")
 (if (file-exists-p custom-file)
